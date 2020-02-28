f18bbd8a8b21f4989b74b384c6ec3274e15b0ca8
damiancclarke <damian.clarke@protonmail.com>
2020-02-27 23:51:53 -0300

    Adding value function code

---
+++ class6/ConvergeGraph.m
@@ -0,0 +1,23 @@
+conv = 100;
+crit = 1e-2;
+
+K = linspace(1e-6,100,1000);
+V = zeros(1000,1);
+axis([0 100 -18 10])
+hold on
+
+cc=hot(70);
+Iter = 0;
+
+while conv>crit
+	Iter = Iter+1
+	[TV opt] = Iterate_VF(V,100);
+	conv = max(abs(TV-V))
+	plot(K,TV, 'color', cc(Iter,:))
+	V = TV;
+end
+
+xlabel('Amount of Capital', 'FontSize', 12)
+ylabel('Value Function', 'FontSize', 12)
+title('Value Function Iteration', 'FontSize', 14)
+
